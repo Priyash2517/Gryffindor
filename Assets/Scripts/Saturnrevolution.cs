a43a787494fcfb531007b50de6f07094bb0cec7c
Priyash Garg <priyash634@gmail.com>
2020-08-13 00:29:30 +0530

    space skybox

--- Assets/Scripts/Saturnrevolution.cs
+++ Assets/Scripts/Saturnrevolution.cs
@@ -13,6 +13,6 @@ public class Saturnrevolution : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.1f, 0);
+        transform.Rotate(0, 0.01f, 0);
     }
 }
